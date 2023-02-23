6d6dde60bc2f642b2c2b25f047deb4bc7a1187c5
peterradovich <peterradovich@localhost>
2023-02-23 15:40:26 -0600

    hi

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,30 @@
 package studio4;
 
 import java.awt.Color;
+import java.util.Scanner;
 
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setPenColor(0, 0, 255);
+		StdDraw.filledRectangle(0.5, 0.5, 0.5, 0.25);
+		StdDraw.setPenColor(25,0,0);
+		StdDraw.filledRectangle(0.5, 0.5, 0.25, 0.25);
+		StdDraw.setPenColor(0,25,0);
+		//StdDraw.filledRectangle(0.25, 0.25, 0.125, 0.125);
+		//StdDraw.filledRectangle(0.25, 0.75, 0.125, 0.125);
+		//StdDraw.filledRectangle(0.75, 0.75, 0.125, 0.125);
+		//StdDraw.filledRectangle(0.75, 0.25, 0.125, 0.125);
+		StdDraw.setPenColor(125,15,190);
+		//StdDraw.filledEllipse(0.25, 0.25, 0.125, 0.125);
+		//StdDraw.filledEllipse(0.25, 0.75, 0.125, 0.125);
+		//StdDraw.filledEllipse(0.75, 0.25, 0.125, 0.125);
+		//StdDraw.filledEllipse(0.75, 0.75, 0.125, 0.125);
+	StdDraw.setPenRadius(0.0525);
+		StdDraw.line(.5, 0.4, 0.5, 0.6);
+		StdDraw.filledEllipse(0.5, 0.5, 0.125, 0.125);
+		
 		
 	}
 }
